3cad263d385f9c6a818c8abd911c9eebabafea75
Perla Abdallah <88480955+perla2000@users.noreply.github.com>
2022-03-29 12:38:02 +0300

    Update HelloServlet.java

--- EJB-SessionBean1/src/main/java/com/example/ejbsessionbean/HelloServlet.java
+++ EJB-SessionBean1/src/main/java/com/example/ejbsessionbean/HelloServlet.java
@@ -7,11 +7,10 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+    @EJB
     private HelloResource bean= new HelloResourceBean();
 
-    public void init() {
-        message = bean.hello();
-    }
+
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
@@ -19,7 +18,7 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body bgcolor=\"floralwhite\">");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + bean.hello() + "</h1>");
         out.println("</body></html>");
     }
 
